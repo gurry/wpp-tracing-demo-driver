955883415d18482abe2048a73c80a8aded901a1b
Gurinder Singh <frederick.the.fool@gmail.com>
2026-08-03 17:43:36 +0530

    Added/improved some comments

--- src/lib.rs
+++ src/lib.rs
@@ -5,29 +5,17 @@ use wdf::{
     driver_entry, wpp_control_guids, DriverObject, IoQueue, NtResult, Request, UnicodeString,
 };
 
-// WPP tracing requires you to declare what's called a "Control GUID".
-// That is what this macro declares.
-// The Control GUID is emitted in every trace call at run time (i.e. when
-// the driver is running) and the same GUID is embedded in the decoding
-// metadata in the PDB. The GUID is used by the decoding tools to
-// match the the runtime traces with the metadata from the PDB and 
-// produces human-readable trace messages.
+// Boilerplate declaration required by the tracing framework.
+// SampleDriver: Name of the driver used when registering
+//               with the tracing framework
+// GUID:        Identity of the driver in the tracing framework
+// IO and INIT: "keywords" used to categorize trace messages
 wpp_control_guids!(
     SampleDriver 91e9d7e8-fc4b-4bb5-bfc7-b588a9dc0ca5 {
-        // IO and INIT are what are called "keywords" 
-        // They are used in trace statements to indicate the
-        // category of the trace message.
         IO,
-        INIT
+        INIT,
     }
 );
-
-// The entry-point of the driver
-#[driver_entry(trace_providers(SampleDriver))]
-fn driver_entry(_driver_object: &mut DriverObject, _registry_path: &UnicodeString) -> NtResult<()> {
-    // Set up I/O queues here
-    Ok(())
-}
 
 // A driver callback that will be called by the OS when there's
 // an I/O request
@@ -35,14 +23,26 @@ fn driver_entry(_driver_object: &mut DriverObject, _registry_path: &UnicodeStrin
 fn evt_io_default(_queue: &IoQueue, _request: Request) {
     // Handle IO here...
 
-    // Do some logging
+    // Do some logging (using dummy values)
     let byte_count: usize = 1024;
     let elapsed_ms: f64 = 12.34;
 
     // `trace!()` will expand to a call to `codeview_annotation` which
-    // embeds decoding metadata in the PDB plus a call to 
-    // another function `WppAutolog` which will be executed at
-    // runtime emit the values of `byte_count` and `elapsed_ms`
-    // in the OS's tracing infrastructure
-    trace!(IO, "Bytes {}, duration {} ms", byte_count, elapsed_ms);
+    // embeds decoding metadata into the PDB plus a call to 
+    // another function `etw::write` which will emit the values of
+    // `byte_count` and `elapsed_ms` into the tracing infra at runtime
+    trace!(INFO, IO, "Bytes {}, duration {} ms", byte_count, elapsed_ms);
+    //     ^^^^  ^^  ^^^^^^^^^^^^^^^^^^^^^^^^^^  ^^^^^^^^^^  ^^^^^^^^^^
+    //      |    |             |                     |           |
+    //  Trace  Keyword      Format string           Arg1        Arg2
+    //  Level  (category)
+}
+
+// The entry-point of the driver
+#[driver_entry(trace_providers(SampleDriver))]
+fn driver_entry(_driver_object: &mut DriverObject, _registry_path: &UnicodeString) -> NtResult<()> {
+    // Set up device here...
+    // Set up I/O queues here...
+
+    Ok(())
 }
